65e6be6379215c575d23eff4ec5eedb19e0e3163
Zoltán Raffay <villagerslovak@gmail.com>
2024-03-08 12:32:20 +0100

    cviko 3 uloha 4 - N:N funkcie

--- p32uniNN/src/main/java/vsa/NewMain.java
+++ p32uniNN/src/main/java/vsa/NewMain.java
@@ -8,9 +8,11 @@ package vsa;
 import entities.Kniha;
 import entities.Osoba;
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -28,6 +30,10 @@ public class NewMain {
         kniha.setAutori(new ArrayList<>());
         kniha.getAutori().add(autor1);
         kniha.getAutori().add(autor2);
+        Kniha kniha2 = new Kniha();
+        kniha2.setNazov("jazyk java");
+        kniha2.setAutori(new ArrayList<>());
+        kniha2.getAutori().add(autor2);
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("vsaPU");
         EntityManager em = emf.createEntityManager();
@@ -35,9 +41,10 @@ public class NewMain {
         try {
 
             em.persist(kniha);
+            em.persist(kniha2);
             em.persist(autor1);
             em.persist(autor2);
-            
+
             em.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,6 +52,27 @@ public class NewMain {
         } finally {
             em.close();
         }
+        int id = 1;
+        Long longId = (long) 1;
+        System.out.println("HERE");
+        List<Osoba> autori = nájdiAutorov(longId);
+        for (Osoba o : autori) {
+            System.out.println(o.getMeno());
+        }
+
+        System.out.println("Pocet Knih od" + " Kernighan " + pocetKnih("Kernighan"));
+        System.out.println("Ritchey Kernighan:");
+        List<Kniha> knihy = nájdiKnihy("Kernighan");
+        for (Kniha k : knihy) {
+            System.out.println(k.getNazov());
+        }
+        System.out.println("Pocet Knih od" + " Ritchey " + pocetKnih("Ritchey"));
+        System.out.println("Ritchey KNIHY:");
+        List<Kniha> knihy2 = nájdiKnihy("Ritchey");
+        for (Kniha k : knihy2) {
+            System.out.println(k.getNazov());
+        }
+
     }
 
     public static void persist(Object object) {
@@ -61,5 +89,38 @@ public class NewMain {
             em.close();
         }
     }
-    
+
+    public static List<Osoba> nájdiAutorov(Long idKnihy) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("vsaPU");
+        EntityManager em = emf.createEntityManager();
+        Kniha k = em.find(Kniha.class, idKnihy);
+        return k.getAutori();
+    }
+
+    public static int pocetKnih(String menoAutora) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("vsaPU");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Osoba> osobaQuery = em.createQuery("SELECT o FROM Osoba o WHERE o.meno = :meno", Osoba.class);
+        osobaQuery.setParameter("meno", menoAutora);
+        if (osobaQuery.getResultList().isEmpty()) {
+            return 0;
+        }
+        TypedQuery<Kniha> knihaQuery = em.createQuery("SELECT COUNT(k) FROM Kniha k INNER JOIN Osoba o ON k.autori.meno = :meno GROUP BY k.id", Kniha.class);
+        knihaQuery.setParameter("meno", menoAutora);
+        return knihaQuery.getResultList().size();
+    }
+
+    public static List<Kniha> nájdiKnihy(String menoAutora) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("vsaPU");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Osoba> osobaQuery = em.createQuery("SELECT o FROM Osoba o WHERE o.meno = :meno", Osoba.class);
+        osobaQuery.setParameter("meno", menoAutora);
+        if (osobaQuery.getResultList().isEmpty()) {
+            return null;
+        }
+        TypedQuery<Kniha> knihyQuery = em.createQuery("SELECT DISTINCT k FROM Kniha k INNER JOIN Osoba o ON k.autori.meno = :meno", Kniha.class);
+        knihyQuery.setParameter("meno", menoAutora);
+        return knihyQuery.getResultList();
+    }
+
 }
