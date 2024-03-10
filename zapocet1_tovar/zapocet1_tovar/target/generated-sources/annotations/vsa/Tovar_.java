9f2e9e939e5502249da2afcec3fdf594652bd243
Zoltán Raffay <villagerslovak@gmail.com>
2024-03-10 11:37:05 +0100

    add zapocet1 preparation

---
+++ zapocet1_tovar/zapocet1_tovar/target/generated-sources/annotations/vsa/Tovar_.java
@@ -0,0 +1,15 @@
+package vsa;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2024-03-09T16:37:48")
+@StaticMetamodel(Tovar.class)
+public class Tovar_ { 
+
+    public static volatile SingularAttribute<Tovar, String> nazov;
+    public static volatile SingularAttribute<Tovar, Long> id;
+    public static volatile SingularAttribute<Tovar, Double> cena;
+
+}
